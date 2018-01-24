89606cfcc480f4144287e95b464b2e2abbf34015
Victoria West <victoriawest811@gmail.com>
2018-01-24 16:35:09 -0500

    AND example

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    int a = 2;
+	    int b = 4;
+	    int x = 6;
+	    int y = 1;
+	    System.out.println("a = 2");
+        System.out.println("b = 4");
+        System.out.println("x = 6");
+        System.out.println("y = 1");
+
+        if(a == x && y + y == a ){
+            System.out.println("a == x && y + y == a");
+        }
+
     }
 }
